238a9342aa040bbaad5f7d095ba6ddf7a2e6d075
Hitendra <hitendrasinhrathod.elsner@gmail.com>
2022-10-21 16:58:18 +0530

    IOS Notification Test

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,15 +1,31 @@
 import UIKit
 import Flutter
+import Firebase
 import GoogleMaps
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, MessagingDelegate  {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-      GMSServices.provideAPIKey("AIzaSyCl9GydqOAQRo2ZrjugUq48Jy3VIpx48TM")
+    FirebaseApp.configure()
+    Messaging.messaging().delegate = self
+    GMSServices.provideAPIKey("AIzaSyCl9GydqOAQRo2ZrjugUq48Jy3VIpx48TM")
     GeneratedPluginRegistrant.register(with: self)
+    if #available(iOS 10.0, *) {
+        // For iOS 10 display notification (sent via APNS)
+        UNUserNotificationCenter.current().delegate = self
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: {_, _ in })
+    } else {
+        let settings: UIUserNotificationSettings =
+        UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+        application.registerUserNotificationSettings(settings)
+    }
+    application.registerForRemoteNotifications()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
